4a997874a15bc7945bf659a227f98a64a946a2cb
Juno <138246438+LeeT27@users.noreply.github.com>
2026-04-13 14:33:04 -0500

    Add files via upload

---
+++ Exercise #2/regfile_tb.sv
@@ -0,0 +1,44 @@
+`timescale 1ns/1ps
+
+module regfile_tb;
+    // Signals
+    logic clk;
+    logic write_ctrl;
+    logic [4:0] rs1, rs2, rd;
+    logic [31:0] data;
+    logic [31:0] rv1, rv2;
+
+    // Instantiate DUT
+    regfile dut (
+        .clk(clk),
+        .write_ctrl(write_ctrl),
+        .rs1(rs1),
+        .rs2(rs2),
+        .rd(rd),
+        .data(data),
+        .rv1(rv1),
+        .rv2(rv2)
+    );
+
+    // Clock (10 time unit period)
+    always #5 clk = ~clk;
+
+    initial begin // Initialize 
+        clk = 0;
+        //START WRITING CODE HERE
+        
+        write_ctrl = ;
+        rs1 = ;
+        rs2 = ;
+        rd = ;
+        data = ;
+        #10; // 10ns delay 
+        
+        //Repeat the above lines for every signal change
+
+
+
+        $finish; // End simulation
+    end
+
+endmodule
